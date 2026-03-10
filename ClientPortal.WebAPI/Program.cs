48376f645f6e04feb555feadffbde64801a2be0c
MaxAref07 <makar8777@gmail.com>
2026-03-10 14:09:22 +0100

    feat(infrastructure): Add in-memory project repository

--- ClientPortal.WebAPI/Program.cs
+++ ClientPortal.WebAPI/Program.cs
@@ -1,10 +1,12 @@
 using ClientPortal.Application;
+using ClientPortal.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Register services
 builder.Services.AddOpenApi();
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
