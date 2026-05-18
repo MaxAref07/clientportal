efe4f8f6768fa5cc16dfcfb9ed5ad7a65b2d0e45
MaxAref07 <makar8777@gmail.com>
2026-05-18 13:31:36 +0200

    feat(application): Add CreateFeature command and handler

--- ClientPortal.Application/Features/Commands/CreateFeature/CreateFeatureCommand.cs
+++ ClientPortal.Application/Features/Commands/CreateFeature/CreateFeatureCommand.cs
@@ -1,12 +1,15 @@
+using ClientPortal.Application.DTOs;
+using ClientPortal.Application.Projects.DTOs;
+using ClientPortal.Domain.Entities;
 using ClientPortal.Domain.Enums;
+using MediatR;
 
-namespace ClientPortal.Application.Projects.Commands.CreateFeature;
+namespace ClientPortal.Application.Features.Commands.CreateFeature;
 
-public class CreateFeatureCommand
+public class CreateFeatureCommand : IRequest<FeatureDto>
 {
     public required string Name { get; set; }
-    
     public required string Description { get; set; }
-    
     public FeaturePriority Priority { get; set; }
+    public Guid ProjectId { get; set; }
 }
--- ClientPortal.Application/Features/Commands/CreateFeature/CreateFeatureCommandHandler.cs
+++ ClientPortal.Application/Features/Commands/CreateFeature/CreateFeatureCommandHandler.cs
@@ -1,6 +1,48 @@
-namespace ClientPortal.Application.Projects.Commands.CreateFeature;
+using ClientPortal.Application.DTOs;
+using ClientPortal.Application.Exceptions;
+using ClientPortal.Application.Interfaces;
+using ClientPortal.Application.Projects.DTOs;
+using ClientPortal.Domain.Entities;
+using ClientPortal.Domain.Enums;
+using MediatR;
 
-public class CreateFeatureCommandHandler
+namespace ClientPortal.Application.Features.Commands.CreateFeature;
+
+public class CreateFeatureCommandHandler : IRequestHandler<CreateFeatureCommand, FeatureDto>
 {
+    private readonly IFeatureRepository _featureRepository;
+    private readonly IFeatureReadRepository _readFeatureRepository;
+    private readonly IProjectReadRepository _projectReadRepository;
+
+    public CreateFeatureCommandHandler(IFeatureRepository featureRepository, IFeatureReadRepository readFeatureRepository, IProjectReadRepository projectReadRepository)
+    {
+        _featureRepository = featureRepository;
+        _readFeatureRepository = readFeatureRepository;
+        _projectReadRepository = projectReadRepository;
+    }
     
+    public async Task<FeatureDto> Handle(CreateFeatureCommand request, CancellationToken cancellationToken)
+    {
+        var project = await _projectReadRepository.GetProjectById(request.ProjectId);
+
+        if (project == null)
+            throw new ProjectNotFoundException($"Project with id {request.ProjectId} not found");
+        
+        var features = await _readFeatureRepository.GetFeaturesByProjectId(project.Id);
+        
+        if (features.Count() >= project.ScopeFeatures)
+            throw new FeaturesOutOfScopeException($"Feature scope for project {project.Id} has been exceeded");
+        
+        var feature = new Feature(Guid.NewGuid(), request.Name, request.Description, request.Priority, FeatureStatus.ToDo, request.ProjectId);
+        
+        var createdProject = await _featureRepository.Add(feature);
+
+        return new FeatureDto(
+            createdProject.Id,
+            createdProject.Name, 
+            createdProject.Priority,
+            createdProject.Status,
+            createdProject.Description,
+            createdProject.ProjectId);
+    }
 }
