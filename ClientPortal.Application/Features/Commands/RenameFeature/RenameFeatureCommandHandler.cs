7d16b00b17f5795d1fa0675a1b753414fafb561f
MaxAref07 <makar8777@gmail.com>
2026-06-02 12:05:35 +0200

    feat(application): Implement RenameFeature command and handler

---
+++ ClientPortal.Application/Features/Commands/RenameFeature/RenameFeatureCommandHandler.cs
@@ -0,0 +1,21 @@
+using ClientPortal.Application.DTOs;
+using ClientPortal.Application.Exceptions;
+using ClientPortal.Application.Interfaces;
+using MediatR;
+
+namespace ClientPortal.Application.Features.Commands.RenameFeature;
+
+public class RenameFeatureCommandHandler(IFeatureReadRepository featureReadRepository) : IRequestHandler<RenameFeatureCommand, FeatureDto>
+{
+    public async Task<FeatureDto> Handle(RenameFeatureCommand request, CancellationToken cancellationToken)
+    {
+        var feature = await featureReadRepository.GetFeatureById(request.Id);
+        
+        if (feature == null)
+            throw new FeatureNotFoundException($"Feature with id {request.Id} was not found");
+        
+        feature.Rename(request.NewName);
+        
+        return new FeatureDto(feature.Id, feature.Name, feature.Priority, feature.Status, feature.Description, feature.ProjectId);
+    }
+}
