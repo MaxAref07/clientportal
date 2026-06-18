bab140b284b3854092131ef52525abac3b848f02
MaxAref07 <makar8777@gmail.com>
2026-06-18 13:25:57 +0200

    feat(application): Implement cascade features deletion when deleting a project

--- ClientPortal.Application/Projects/Commands/DeleteProject/DeleteProjectCommandHandler.cs
+++ ClientPortal.Application/Projects/Commands/DeleteProject/DeleteProjectCommandHandler.cs
@@ -5,7 +5,10 @@ using MediatR;
 
 namespace ClientPortal.Application.Projects.Commands.DeleteProject;
 
-public class DeleteProjectCommandHandler(IProjectReadRepository projectReadRepository, IProjectRepository projectRepository) : IRequestHandler<DeleteProjectCommand>
+public class DeleteProjectCommandHandler(IProjectReadRepository projectReadRepository,
+    IProjectRepository projectRepository,
+    IFeatureReadRepository featureReadRepository,
+    IFeatureRepository featureRepository) : IRequestHandler<DeleteProjectCommand>
 {
     public async Task Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
     {
@@ -13,6 +16,14 @@ public class DeleteProjectCommandHandler(IProjectReadRepository projectReadRepos
         
         if (project == null)
             throw new ProjectNotFoundException($"Project with id {request.Id} was not found");
+        
+        var projectFeatures = await featureReadRepository.GetFeaturesByProjectId(project.Id);
+
+        if (projectFeatures.Count != 0)
+        {
+            foreach (var feature in projectFeatures)
+                await featureRepository.Delete(feature.Id);
+        }
         
         await projectRepository.Delete(request.Id);
     }
