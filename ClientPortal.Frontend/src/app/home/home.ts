eea50fbcbdf5b59d84c7f1ac17fb1dc2507e86fa
MaxAref07 <makar8777@gmail.com>
2026-06-20 13:11:57 +0200

    feat(frontend): Implement getting projects from service

--- ClientPortal.Frontend/src/app/home/home.ts
+++ ClientPortal.Frontend/src/app/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { ProjectService } from '../services/project.service';
+import { ProjectDto } from '../types/project.types';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  private projectService = inject(ProjectService);
+
+  projects = signal<ProjectDto[]>([])
+
+  ngOnInit() {
+    this.projectService.getAllProjects().subscribe({
+      next: (data) => {
+        this.projects.set(data);
+      },
+      error: err => {
+        console.error("Error getting projects", err);
+      }
+    });
+  }
+}
