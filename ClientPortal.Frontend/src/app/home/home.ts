48dd439e64ffbc3bf23eded3c9384f67e50d6e2d
MaxAref07 <makar8777@gmail.com>
2026-06-26 12:15:18 +0200

    feat(frontend): Add create project form to Home

--- ClientPortal.Frontend/src/app/home/home.ts
+++ ClientPortal.Frontend/src/app/home/home.ts
@@ -1,27 +1,85 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { ProjectService } from '../services/project.service';
-import { ProjectDto } from '../types/project.types';
+import { CreateProjectDto, ProjectDto } from '../types/project.types';
 import { RouterLink, RouterModule } from '@angular/router';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home implements OnInit {
   private projectService = inject(ProjectService);
+  private formBuilder = inject(FormBuilder);
 
-  projects = signal<ProjectDto[]>([])
+  projects = signal<ProjectDto[]>([]);
+  isFormOpen = signal<boolean>(false);
+  isSubmitting = signal<boolean>(false);
+  submitError = signal<string | null>(null);
+
+  createProjectForm = this.formBuilder.group({
+    name: this.formBuilder.control('', { validators: [Validators.required], nonNullable: true }),
+    description: this.formBuilder.control('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    scopeFeatures: this.formBuilder.control(1, {
+      validators: [Validators.required, Validators.min(1)],
+      nonNullable: true,
+    }),
+  });
 
   ngOnInit() {
     this.projectService.getAllProjects().subscribe({
-      next: (data) => {
-        this.projects.set(data);
+      next: (data) => this.projects.set(data),
+      error: (err) => console.error('Error getting projects', err),
+    });
+  }
+
+  toggleForm() {
+    this.isFormOpen.update((x) => !x);
+    if (!this.isFormOpen()) {
+      this.resetFormState();
+    }
+  }
+
+  resetFormState() {
+    this.createProjectForm.reset({ name: '', description: '', scopeFeatures: 1 });
+    this.submitError.set(null);
+  }
+
+  onSubmit() {
+    if (this.createProjectForm.invalid) {
+      this.createProjectForm.markAllAsTouched();
+      return;
+    }
+
+    this.isSubmitting.set(true);
+    this.submitError.set(null);
+
+    const formRaw = this.createProjectForm.getRawValue();
+
+    const newProject: CreateProjectDto = {
+      name: formRaw.name,
+      description: formRaw.description,
+      scopeFeatures: formRaw.scopeFeatures,
+    };
+
+    this.projectService.createProject(newProject).subscribe({
+      next: (createdProject) => {
+        this.projects.update((list) => [createdProject, ...list]);
+
+        this.isFormOpen.set(false);
+        this.isSubmitting.set(false);
+        this.resetFormState();
       },
-      error: err => {
-        console.error("Error getting projects", err);
-      }
+      error: (err) => {
+        this.submitError.set('Failed to create project. Please try again.');
+        this.isSubmitting.set(false);
+      },
     });
   }
 }
