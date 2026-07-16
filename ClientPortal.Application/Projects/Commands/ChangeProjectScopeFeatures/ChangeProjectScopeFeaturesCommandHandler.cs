62a323b89f3aded3f995485df1b34ecf0ccd8088
MaxAref07 <makar8777@gmail.com>
2026-07-16 13:02:48 +0200

    feat(application): Load only project's features when changing scope

--- ClientPortal.Application/Projects/Commands/ChangeProjectScopeFeatures/ChangeProjectScopeFeaturesCommandHandler.cs
+++ ClientPortal.Application/Projects/Commands/ChangeProjectScopeFeatures/ChangeProjectScopeFeaturesCommandHandler.cs
@@ -14,25 +14,23 @@ public class ChangeProjectScopeFeaturesCommandHandler(IProjectReadRepository pro
         
         if (project == null)
             throw new ProjectNotFoundException($"Project with id {request.Id} was not found");
-
-        var features = await featureReadRepository.GetFeatures();
-        var projectFeatures = features.Count(f => f.ProjectId == project.Id);
         
-        if (features.Count(f => f.ProjectId == project.Id) > request.NewScopeFeatures)
-            throw new MinimumFeatureScopeException(projectFeatures, request.NewScopeFeatures);
+        var projectFeatures = await featureReadRepository.GetFeaturesByProjectId(project.Id);
+        var projectFeaturesCount = projectFeatures.Count;
+        if (projectFeaturesCount > request.NewScopeFeatures)
+            throw new MinimumFeatureScopeException(projectFeaturesCount, request.NewScopeFeatures);
         
         project.ChangeScope(request.NewScopeFeatures);
         
         await unitOfWork.SaveChangesAsync(cancellationToken);
 
-        var currentFeaturesCount = projectFeatures;
-        var completedFeaturesCount = features.Count(x => x.Status == FeatureStatus.Done && x.ProjectId == project.Id);
+        var completedFeaturesCount = projectFeatures.Count(x => x.Status == FeatureStatus.Done);
         
         return new ProjectDto(project.Id,
             project.Name,
             project.Description,
             project.ScopeFeatures,
-            currentFeaturesCount,
+            projectFeaturesCount,
             completedFeaturesCount);
     }
 }
